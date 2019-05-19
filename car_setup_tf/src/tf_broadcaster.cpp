6b452e929c24e04c03fb3982c536d8caba10e8dd
Jon Eivind Stranden <joneivinds@gmail.com>
2019-05-19 16:43:28 +0200

    Add files via upload

---
+++ car_setup_tf/src/tf_broadcaster.cpp
@@ -0,0 +1,19 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "car_tf_publisher");
+  ros::NodeHandle n;
+
+  ros::Rate r(100);
+
+  tf::TransformBroadcaster broadcaster;
+
+  while(n.ok()){
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 3.14, 1), tf::Vector3(0.28, 0.0, 0.27)),
+        ros::Time::now(),"base_link", "laser"));
+    r.sleep();
+  }
+}
